05d5d014ee3fff52ee4eda09b1a51f6964223f1e
Michał Fudała <michfudala@gmail.com>
2025-02-06 11:02:45 +0100

    Initialize USB files

---
+++ usb/usb_lib.c
@@ -0,0 +1,3 @@
+#include "usb_lib.h"
+
+
---
+++ usb/usb_lib.h
@@ -0,0 +1,14 @@
+/*
+    Configuration for USB CH32V203
+
+    https://github.com/m-fudala
+*/
+
+#ifndef USB_LIB_H_
+#define USB_LIB_H_
+
+#include "ch32v20x.h"
+
+
+
+#endif
