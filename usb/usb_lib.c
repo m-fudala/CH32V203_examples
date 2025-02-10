3fcd89e9501b329f58c2096145bb7649c0b97e1b
Michał Fudała <michfudala@gmail.com>
2025-02-10 08:03:36 +0100

    Endpoint0 and 1 configuration

--- usb/usb_lib.c
+++ usb/usb_lib.c
@@ -1,5 +1,10 @@
 #include "usb_lib.h"
 
+__attribute__((__aligned__(4))) int endpoint0_buffer
+                                    [USBFS_DEFAULT_BUFFER_SIZE];
+__attribute__((__aligned__(4))) int endpoint1_buffer
+                                    [USBFS_DEFAULT_BUFFER_SIZE];
+
 void usb_init(void) {
     // GPIO settings
     RCC->APB2PCENR |= RCC_IOPBEN;   // enable port B clock
@@ -35,6 +40,29 @@ void usb_init(void) {
     USBFSD->BASE_CTRL &= ~(USBFSD_RB_UC_RESET_SIE | USBFSD_RB_UC_CLR_ALL);
 }
 
+void configure_endpoint0(void) {
+    USBFSD->UEP0_DMA = endpoint0_buffer;
+
+    USBFSD->UEP0_TX_CTRL |= RB_UEP_T_AUTO_TOG | TX_ANSWER_NAK;
+    USBFSD->UEP0_RX_CTRL |= RB_UEP_R_AUTO_TOG | RX_ANSWER_NAK;
+}
+
+void deconfigure_endpoint1(void) {
+    USBFSD->UEP4_1_MOD = 0;
+    USBFSD->UEP1_TX_CTRL = 0;
+    USBFSD->UEP1_RX_CTRL = 0;
+}
+
+void configure_endpoint1(void) {
+    USBFSD->UEP1_DMA = endpoint1_buffer;
+
+    // enable IN endpoint
+    USBFSD->UEP4_1_MOD |= RB_UEP1_TX_EN;
+
+    USBFSD->UEP1_TX_CTRL |= RB_UEP_T_AUTO_TOG | TX_ANSWER_NAK;
+    USBFSD->UEP1_RX_CTRL |= RB_UEP_R_AUTO_TOG | RX_ANSWER_NAK;
+}
+
 void USB_HP_CAN1_TX_IRQHandler(void) {
 
 }
@@ -45,8 +73,15 @@ void USB_LP_CAN1_RX0_IRQHandler(void) {
 
 void USBFS_IRQHandler(void) {
     volatile int usb_interrupt_flags = USBFSD->INT_FG;  // read the status
+
+    if (usb_interrupt_flags & RB_UIF_BUS_RST) {
+        // TODO: deconfigure more endpoints
+        deconfigure_endpoint1();        
+
+        USBFSD->INT_FG |= RB_UIF_BUS_RST;   // clear interrupt
+    }
 }
 
 void USBFSWakeUp_IRQHandler(void) {
-    
+
 }
--- usb/usb_lib.h
+++ usb/usb_lib.h
@@ -16,6 +16,8 @@
 
 #include "ch32v20x.h"
 
+#define USBFS_DEFAULT_BUFFER_SIZE       64
+
 // R8_USB_CTRL bits
 #define USBFSD_USBD_EN_PULLUP_EN        (2 << 4)
 #define USBFSD_RB_UC_INT_BUSY           (1 << 3)
@@ -35,7 +37,35 @@
 // R8_USB_DEV_AD
 #define MASK_USB_ADDR                   (0x7F)
 
+// R8_USB_INT_FG bits
+#define RB_UIF_BUS_RST                  (1 << 0)
+
+// R8_UEPn_TX_CTRL bits
+#define RB_UEP_T_AUTO_TOG               (1 << 3)
+#define RB_UEP_T_TOG                    (1 << 2)
+#define TX_D0_D1_READY_ACK_EXPECTED     (0x0)
+#define TX_D0_D1_REPLY_NO_RESP_EXPECTED (0x1)
+#define TX_ANSWER_NAK                   (0x2)
+#define TX_ANSWER_STALL                 (0x3)
+
+// R8_UEPn_RX_CTRL bits
+#define RB_UEP_R_AUTO_TOG               (1 << 3)
+#define RB_UEP_R_TOG                    (1 << 2)
+#define RX_ANSWER_ACK                   (0x0)
+#define RX_TIMEOUT_NO_RESPONSE          (0x1)
+#define RX_ANSWER_NAK                   (0x2)
+#define RX_ANSWER_STALL                 (0x3)
+
+// R8_UEP4_1_MOD bits
+#define RB_UEP1_RX_EN                   (1 << 7)
+#define RB_UEP1_TX_EN                   (1 << 6)
+#define RB_UEP1_BUF_MOD                 (1 << 4)
+
 void usb_init(void);
+
+void configure_endpoint0(void);
+void deconfigure_endpoint1(void);
+void configure_endpoint1(void);
 
 void USB_HP_CAN1_TX_IRQHandler(void) __attribute__((interrupt()));
 void USB_LP_CAN1_RX0_IRQHandler(void) __attribute__((interrupt()));
